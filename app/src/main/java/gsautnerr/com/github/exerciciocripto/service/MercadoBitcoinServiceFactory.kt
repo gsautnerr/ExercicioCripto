0ffb2aa672a90b5e5a62802608a18645e38db86e
Laboratório FIAP <labsfiap@fiap.com.br>
2024-10-17 22:40:14 -0300

    terceiro commit

---
+++ app/src/main/java/gsautnerr/com/github/exerciciocripto/service/MercadoBitcoinServiceFactory.kt
@@ -0,0 +1,15 @@
+import gsautnerr.com.github.exerciciocripto.service.MercadoBitcoinService
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+class MercadoBitcoinServiceFactory {
+
+    fun create(): MercadoBitcoinService {
+        val retrofit = Retrofit.Builder()
+            .baseUrl("https://www.mercadobitcoin.net/")
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+
+        return retrofit.create(MercadoBitcoinService::class.java)
+    }
+}
